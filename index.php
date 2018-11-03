d120511b25223987335df0b1eb86140e38411f7c
Maurice MJ de Jong <meauris@outlook.com>
2018-11-03 13:41:33 +0100

    Update index.php
    just for testing

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Hello World / System Info</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; phpinfo(); ?> 
+ <?php echo '<p>Hello World2</p>'; phpinfo(); ?> 
  </body>
 </html>
